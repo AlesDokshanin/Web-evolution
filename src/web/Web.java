fbe15dd2b0a6080bfad46a322a8499b7dc445b49
Ales Dokshanin <alesdokshanin@gmail.com>
2014-11-25 19:34:17 +0300

    Almost finished skeleton generation. Still need some fixes.

--- src/web/Web.java
+++ src/web/Web.java
@@ -2,6 +2,7 @@ package web;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Web {
@@ -24,6 +25,25 @@ public class Web {
     private int height;
     private Point center;
 
+    private class PolarPoint implements Comparable<PolarPoint> {
+        public double angle;
+        public int distance;
+
+        public PolarPoint(double angle, int distance) {
+            this.angle = angle;
+            this.distance = distance;
+        }
+
+        public Point getCartesianPoint() {
+            return new Point((int)(Math.cos(angle) * distance), (int)(Math.sin(angle) * distance));
+        }
+
+        @Override
+        public int compareTo(PolarPoint polarPoint) {
+            return Double.compare(angle, polarPoint.angle);
+        }
+    }
+
     public Web(int width, int height) {
         innerLinesCoors = new ArrayList<Point>();
         skeletonCoords = new ArrayList<Point>();
@@ -35,24 +55,45 @@ public class Web {
     }
 
     private void generate() {
-        skeletonCoords.clear();
-        innerLinesCoors.clear();
+        generateSkeleton();
+        generateInnerLines();
+    }
 
+    private void generateInnerLines() {
+        innerLinesCoors.clear();
+    }
+
+    private void generateSkeleton() {
+        skeletonCoords.clear();
+
+        ArrayList<PolarPoint> skeletonPolarPoints = new ArrayList<PolarPoint>();
         for (int i = 0; i < skeletonSides; i++) {
             Random r = new Random();
             double angle = r.nextDouble() * 2 * Math.PI; // Generate angle in range [0, 2 * PI]
-            int distance_min = Math.min(width, height) / 20;
+            int distance_min = Math.min(width, height) / 5;
             int distance_max = 0;
-            Point bound = new Point();
+            Point bound;
             while(distance_max <= distance_min) {
                 bound = new Point((int) (0.5 * width * Math.cos(angle)),(int) (0.5 * height * Math.sin(angle)));
                 distance_max = (int) bound.distance(0, 0);
             }
-
             int distance = (int) (distance_min + (distance_max - distance_min) * r.nextDouble());
-            double k = (double) distance / distance_max;
-            Point p = new Point((int) (k * bound.x) + center.x, (int) (k * bound.y) + center.y);
+
+            // TODO prevent generating points close to each other and with same (almost) angle.
+            skeletonPolarPoints.add(new PolarPoint(angle, distance));
+        }
+
+        Collections.sort(skeletonPolarPoints);
+        for(int i = 0; i < skeletonPolarPoints.size(); i++) {
+            Point p = skeletonPolarPoints.get(i).getCartesianPoint();
+            p.x += center.x;
+            p.y += center.y;
             skeletonCoords.add(p);
+        }
+
+        Polygon skeleton = getPolygonFromPointsArray(skeletonCoords);
+        if(!skeleton.contains(center)) {
+            generateSkeleton();
         }
     }
 
@@ -67,8 +108,8 @@ public class Web {
     }
 
     private void drawSkeleton(Graphics2D g) {
-        /*Polygon p = getPolygonFromPointsArray(skeletonCoords);
-        g.drawPolygon(p);*/
+        Polygon polygon = getPolygonFromPointsArray(skeletonCoords);
+        g.drawPolygon(polygon);
 
         for(Point p : skeletonCoords) {
             g.fillOval(p.x - 5, p.y - 5, 10, 10);
