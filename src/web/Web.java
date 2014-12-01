eaca2b51d6a70935541c953bfa295bd145b7d9ad
Ales Dokshanin <alesdokshanin@gmail.com>
2014-12-01 22:07:05 +0300

    Finished web generation.

--- src/web/Web.java
+++ src/web/Web.java
@@ -1,39 +1,12 @@
 package web;
 
+import javax.naming.OperationNotSupportedException;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
 public class Web {
-
-    private static int skeletonSides = 10;
-    private static int innnerLinesCount = 5;
-
-    public static int width = 600;
-    public static int height = 600;
-    private Point center = new Point(width / 2, height / 2);
-
-    private static double minAngleBetweenSkeletonLines = 2 * Math.PI / (3 * skeletonSides);
-    private static int minSkeletonDistance = Math.min(height, width) / 5;
-    private static int minInnerLineDistance = Math.min(height, width) / 35;
-
-    public void setSkeletonSides(int sides) throws IllegalArgumentException {
-        if (sides <= 2)
-            throw new IllegalArgumentException("Number of web skeleton's sides should be more than 2.");
-        skeletonSides = sides;
-    }
-
-    private Polygon skeletonPolygon;
-
-    private ArrayList<Polygon> innerLinesPolygons;
-
-    private ArrayList<PolarPoint> skeletonPoints;
-
-    int generation = 0;
-
-    double efficiency = 0;
-
 
     private class PolarPoint implements Comparable<PolarPoint> {
         double angle;
@@ -54,8 +27,91 @@ public class Web {
         }
     }
 
+    private class WebInnerLine {
+        private ArrayList<PolarPoint> list;
+        private Polygon polygon = null;
+
+        public WebInnerLine() throws OperationNotSupportedException {
+            list = new ArrayList<PolarPoint>();
+            generateInnerLine();
+        }
+
+        public Polygon getPolygon() {
+            if (polygon == null)
+                generatePolygon();
+            return polygon;
+        }
+
+        private void generatePolygon() {
+            int n = list.size();
+            int[] xPoints = new int[n];
+            int[] yPoints = new int[n];
+            for (int i = 0; i < n; i++) {
+                Point p = list.get(i).getCartesianPoint();
+                xPoints[i] = p.x;
+                yPoints[i] = p.y;
+            }
+            polygon = new Polygon(xPoints, yPoints, n);
+            polygon.translate(center.x, center.y);
+        }
+
+        private void generateInnerLine() throws OperationNotSupportedException {
+            Random r = new Random();
+            for (int i = 0; i < skeletonSides; i++) {
+                int lowerBound = minInnerLineDistance;
+                if(!innerLines.isEmpty()) {
+                    lowerBound += innerLines.get(innerLines.size() - 1).list.get(i).distance;
+                }
+                int maxDistance = skeletonPoints.get(i).distance - minInnerLineDistance;
+
+                if(lowerBound > maxDistance)
+                    throw new OperationNotSupportedException("Can't generate inner line point. Min distance > Max distance ");
+
+                int upperBound = Math.min(maxDistance, lowerBound + (int) (innerLinesDispersionCoeff * minInnerLineDistance));
+                double angle = skeletonPoints.get(i).angle;
+
+                int distance = (int) (lowerBound + r.nextDouble() * (upperBound - lowerBound));
+                list.add(new PolarPoint(angle, distance));
+            }
+            innerLines.add(this);
+            generatePolygon();
+        }
+    }
+
+    public static int width = 600;
+    public static int height = 600;
+
+    public void setSkeletonSides(int sides) throws IllegalArgumentException {
+        if (sides <= 2)
+            throw new IllegalArgumentException("Number of web skeleton's sides should be more than 2.");
+        skeletonSides = sides;
+    }
+
+    private static int skeletonSides = 10;
+    private static int innnerLinesCount = 5;
+
+    private Point center = new Point(width / 2, height / 2);
+
+    private static double minAngleBetweenSkeletonLines = 2 * Math.PI / (3 * skeletonSides);
+    private static int minSkeletonDistance = Math.min(height, width) / 5;
+    private static int minInnerLineDistance = Math.min(height, width) / 50;
+    private static int minSkeletonDistanceFromCenter = 2 * minSkeletonDistance;
+
+    private static double innerLinesDispersionCoeff = 4.0;
+
+    private Polygon skeletonPolygon;
+    private ArrayList<Polygon> innerLinesPolygons;
+
+    private ArrayList<PolarPoint> skeletonPoints;
+    private ArrayList<WebInnerLine> innerLines;
+
+    int generation = 0;
+
+    double efficiency = 0;
+
     public Web() {
         skeletonPoints = new ArrayList<PolarPoint>();
+        innerLines = new ArrayList<WebInnerLine>();
 
         center = new Point(width / 2, height / 2);
         generate();
@@ -67,6 +123,13 @@ public class Web {
     }
 
     private void generateInnerLines() {
+        innerLines.clear();
+        try {
+            while(true)
+                innerLines.add(new WebInnerLine());
+        } catch (OperationNotSupportedException e) {
+            // Do nothing. Exception means we can't add more inner lines to our web.
+        }
 
     }
 
@@ -83,7 +146,7 @@ public class Web {
 
         Collections.sort(skeletonPoints);
 
-        updateSkeletonPolygon();
+        generateSkeletonPolygon();
         skeletonPolygon.translate(center.x, center.y);
 
         if (!skeletonIsValid()) {
@@ -101,7 +164,9 @@ public class Web {
             maxDistance = (int) bound.distance(0, 0);
         }
 
-        int distance = (int) (minSkeletonDistance + (maxDistance - minSkeletonDistance) * random.nextDouble());
+
+
+        int distance = (int) (minSkeletonDistanceFromCenter + (maxDistance - minSkeletonDistanceFromCenter) * random.nextDouble());
         return new PolarPoint(angle, distance);
     }
 
@@ -110,13 +175,17 @@ public class Web {
             if (Math.abs(p.angle - listPoint.angle) < minAngleBetweenSkeletonLines)
                 return false;
             int distance = (int) (p.getCartesianPoint().distance(listPoint.getCartesianPoint()));
-            if (distance <= minSkeletonDistance)
+            if (p.distance <= minSkeletonDistance)
                 return false;
         }
         return true;
     }
 
     private boolean skeletonIsValid() {
+        return skeletonContainsCenter();
+    }
+
+    private boolean skeletonContainsCenter() {
         int shift = Math.min(width, height) / 10;
         for (int dx = -1; dx <= 1; dx++) {
             for (int dy = -1; dy <= 1; dy++) {
@@ -126,13 +195,12 @@ public class Web {
                 }
             }
         }
-
         return true;
     }
 
     void draw(Graphics2D g) {
         drawSkeleton(g);
-        drawLines(g);
+        drawInnerLines(g);
         drawCenterPoint(g);
     }
 
@@ -150,7 +218,7 @@ public class Web {
         }
     }
 
-    private void updateSkeletonPolygon() {
+    private void generateSkeletonPolygon() {
         skeletonPolygon = getPolygonFromPolarPointsList(skeletonPoints);
     }
 
@@ -167,8 +235,9 @@ public class Web {
         return new Polygon(xPoints, yPoints, list.size());
     }
 
-    private void drawLines(Graphics2D g) {
-
+    private void drawInnerLines(Graphics2D g) {
+        for(WebInnerLine line : innerLines)
+            g.drawPolygon(line.getPolygon());
     }
 
     private Polygon getPolygonFromPointsArray(ArrayList<Point> list) {
